09ae84f0636a026891238f7f840418d5773b9970
Tyler Long <blackbelt2817@yahoo.com>
2018-03-20 16:24:51 -0400

    AHHHHH EVERYTHING IS BROKEN

--- src/App.js
+++ src/App.js
@@ -3,32 +3,41 @@ import './App.css';
 import { Button } from 'react-bootstrap';
 import ShowChord from './ShowChord';
 import HearChord from './HearChord';
+import axios from 'axios';
 
 class App extends Component {
   constructor(props) {
     super(props);
+    console.log('initialized this.state!');
     this.state = {
       chordname: '',
-      chord1: 'a',
+      chord1: 'A',
       chord2: '',
-      chord3: 'maj'
+      chord3: 'm'
     };
     this.handleChordChange = this.handleChordChange.bind(this);
-    this.printChord = this.printChord.bind(this);
   }
 
-  handleChordChange() {
-    console.log(this.state.chordname);
-    this.setState({ chordname: this.state.chord1 + this.state.chord2 + this.state.chord3 });
-  }
+  
 
-  printChord() {
-    console.log(this.state.chordname);
+  handleChordChange(stateVarThatWillChange, newVal) {
+    console.log(this.state[stateVarThatWillChange] + ' will now be ' + newVal );
+    this.setState({ [stateVarThatWillChange]: newVal }, () => {
+      console.log('your old var is now equal to ', this.state[stateVarThatWillChange]);
+      this.forceUpdate();
+    });
   }
 
   componentDidMount() {
-    console.log("Woohoo!");
-    this.setState({ chordname: this.state.chord1 + this.state.chord2 + this.state.chord3 });
+    let chord1 = document.getElementById('chord1').value;
+    let chord2 = document.getElementById('chord2').value;
+    let chord3 = document.getElementById('chord3').value;
+    let chordname = chord1 + chord2 + chord3;
+
+
+    this.setState({ chord1, chord2, chord3, chordname }, () => {
+      this.forceUpdate();
+    });
   }
 
   render() {
@@ -37,53 +46,39 @@ class App extends Component {
         <header className='App-header'>Chord Horde<hr /></header>
         <h3>Pick a chord:</h3>
         <div className='row'>
-          <select onChange={e => {
-            this.setState({ chord1: e.target.value }, () => {
-              this.setState({ chordname: this.state.chord1 + this.state.chord2 + this.state.chord3 }, () => {
-                this.printChord();
-              })
-            })}
-        } id='chord1' style={{width: 300}}>
-            <option value='a'>A</option>
-            <option value='b'>B</option>
-            <option value='c'>C</option>
-            <option value='d'>D</option>
-            <option value='e'>E</option>
-            <option value='f'>F</option>
-            <option value='g'>G</option>
+          <select onChange={e => this.handleChordChange(e.target.id, e.target.value)}
+          id='chord1' style={{width: 300}}>
+            <option value='A'>A</option>
+            <option value='B'>B</option>
+            <option value='C'>C</option>
+            <option value='D'>D</option>
+            <option value='E'>E</option>
+            <option value='F'>F</option>
+            <option value='G'>G</option>
           </select>
-          <select onChange={e => {
-            this.setState({ chord2: e.target.value }, () => {
-              this.setState({ chordname: this.state.chord1 + this.state.chord2 + this.state.chord3 }, () => {
-                this.printChord();
-              })
-            })}
-        } id='chord2'>
+          <select onChange={e => this.handleChordChange(e.target.id, e.target.value)}
+          id='chord2'>
             <option value=''>Natural</option>
             <option value='#'>♯</option>
             <option value='b'>♭</option>
           </select>
-          <select onChange={e => {
-            this.setState({ chord3: e.target.value }, () => {
-              this.setState({ chordname: this.state.chord1 + this.state.chord2 + this.state.chord3 }, () => {
-                this.printChord();
-              })
-            })}
-        } id='chord3'>
+          <select onChange={e => this.handleChordChange(e.target.id, e.target.value)}
+          id='chord3'>
             <option value='maj'>Major</option>
             <option value='min'>Minor</option>
             <option value='aug'>Augmented</option>
             <option value='dim'>Diminished</option>
             <option value='min'>Minor</option>
           </select>
-          <Button onClick={this.handleChordChange}>Submit</Button>
+          <Button onClick={ this.handleChordChange }>Submit</Button>
         </div>
 
         <div style={{paddingTop: 50}}>
-          <ShowChord chord={this.state.chordname} />
+          <ShowChord chord={ this.state.chordname } />
           <br />
-          <HearChord chord={this.state.chordname} />
+          <HearChord chord={ this.state.chordname } />
         </div>
+        
       </div>
     );
   }
--- src/ShowChord.js
+++ src/ShowChord.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 export default class ShowChord extends Component {
 	render() {
+		console.log("RENDER FUNC CALLED", this.props.chord);
 		return (
 			<ins className="scales_chords_api" chord={this.props.chord}></ins>
 		);
